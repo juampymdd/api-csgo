c0371f8a7fb46c4e5ec386b0cb04a1d40e81d4e3
juampymdd <juampymdd@gmail.com>
2023-03-21 18:54:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Conectarse a la API de Steam
+
+
+1. Instalación
+
+```bash
+    npm install 
+```
+
+2. Configuración
+
+- Copiar el archivo .env.template a .env y rellenar los datos de la API de Steam: ``` cp .env.template .env```
+
+```yml
+    API_KEY=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+```
+Podes obtener la api key en ```https://steamcommunity.com/dev/registerkey```
+
+3. Ejecución
+
+```bash
+    # Normal
+    npm start
+    
+    # Development
+    npm run dev
+```
+
---
+++ server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+
+
+//const steam = new SteamWebAPI(process.env.API_KEY);
+
+const app = express();
+
+app.get('/playerinfo/:id', async (req, res) => {
+    const {id} = req.params
+    const data = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.API_KEY}&steamids=${ id }`) //76561198166924729
+    const json = await data.json();
+    res.send(json);
+});
+
+app.get('/playerstats/:id', async (req, res) => {
+
+    //obtengo parametros de la url
+    const {id} = req.params
+
+    
+    const data = await fetch(` http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=730&key=${process.env.API_KEY}&steamid=${id}`) //76561198166924729
+    const json = await data.json();
+    res.send(json);
+})
+
+app.get('/playerfriendlist/:id', async (req, res) => {
+
+    //obtengo parametros de la url
+    const {id} = req.params
+
+    
+    const data = await fetch(` http://api.steampowered.com/ISteamUser/GetFriendList/v1?key=${process.env.API_KEY}&steamid=${id}`) //76561198166924729
+    const json = await data.json();
+    res.send(json);
+})
+
+
+// app.get('/playeritems/:id', async (req, res) => {
+//     //obtengo parametros de la url
+//     const {id} = req.params
+//     const data = await fetch(`http://api.steampowered.com/IEconItems_730/GetPlayerItems/v0001/?key=${process.env.API_KEY}&steamid=${id}&format=json`) //76561198166924729
+//     const json = await data.json();
+//     res.send(json);
+// })
+
+
+app.listen(5500, () => {
+    console.log('Example app listening on port 3000!');
+});
+
